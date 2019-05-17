7c343cc55daa415682e27b34c5fc20d424396fe1
Ben Strong <whicker86@gmail.com>
2019-05-16 18:52:58 -0600

    password check finished

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,9 +2,10 @@
   <div>
     <h2>Register</h2>
     <input placeholder="Username" v-model="username"/>
-    <input type="password" placeholder="Password"/>
-    <input type="password" placeholder="Password" v-on:input="handleInputs"/>
-    <button @click="showState">Register Account</button>
+    <input type="password" placeholder="Password" v-model="password"/>
+    <input type="password" placeholder="Password" v-model="checkPassword"/>
+    <!-- <h2 v-if="!passwordMatch">password doesn't match</h2> -->
+    <button :disabled="!passwordMatch">Register Account</button>
     <p>{{this.username}}</p>
   </div>
 </template>
@@ -17,15 +18,33 @@ export default {
     return {
       username: '',
       password:'',
-      checkPassword:''
+      checkPassword:'',
+      passwordMatch:false
     }
   },
   
   methods: {
-    showState(){
-      console.log(this.username)
-    }
-  }
+    async handleRegister(){
+      let user = {
+        username: this.username,
+        password: this.password
+      }
+    }      
+  },
+  watch: {
+        checkPassword: function(val) {
+          console.log(this.checkPassword)
+          this.password === this.checkPassword && this.checkPassword.length > 0 ? this.passwordMatch = true : this.passwordMatch = false
+          console.log(this.passwordMatch)
+        }
+      }
+
 
 }
 </script>
+<style scoped>
+h2 {
+  margin: 50px;
+  background: grey;
+}
+</style>
